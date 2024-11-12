9bc642e1505f273beac294a0185948512e98f936
David Štafa <david.stafa@gmail.com>
2024-11-12 20:59:04 +0100

    feat: feat: update SeatMap + passing data to and rendering data in  Seat component

--- src/components/Seat.tsx
+++ src/components/Seat.tsx
@@ -4,18 +4,29 @@ import {
 	PopoverContent,
 	PopoverTrigger,
 } from "@/components/ui/popover.tsx";
-import { cn } from "@/lib/utils.ts";
+import { cn, formatCurrency } from "@/lib/utils.ts";
+import { SeatType, TicketType } from "@/types/types";
 import { X } from "lucide-react";
 import React from "react";
 
 interface SeatProps extends React.HTMLAttributes<HTMLElement> {
 	disabled: boolean | undefined;
 	seatNumber: number;
+	seatData?: SeatType;
+	ticketTypes?: TicketType;
+	rowLetter: string;
 }
 
 export const Seat = React.forwardRef<HTMLDivElement, SeatProps>(
 	(props, ref) => {
 		const isInCart = false;
+		// get correct ticket type according to seat's ticketTypeId
+		// run the function only if both seatData and ticketTypes are true
+		const ticketData =
+			props.seatData &&
+			props.ticketTypes &&
+			getTicketData(props.seatData.ticketTypeId, props.ticketTypes);
+
 		return (
 			<Popover>
 				<PopoverTrigger disabled={props.disabled} className="m-auto">
@@ -29,12 +40,25 @@ export const Seat = React.forwardRef<HTMLDivElement, SeatProps>(
 						)}
 						ref={ref}
 					>
-						<span className="text-xs font-medium">{props.disabled ? <X size="20px" strokeWidth="1px"/> : props.seatNumber}</span>
+						{/* rendering X icon if seat is disabled or seat number if seat is free */}
+						<span className="text-xs font-medium">
+							{props.disabled ? (
+								<X size="20px" strokeWidth="1px" />
+							) : (
+								props.seatNumber
+							)}
+						</span>
 					</div>
 				</PopoverTrigger>
 				<PopoverContent>
-					<pre>{JSON.stringify({ seatData: null }, null, 2)}</pre>
-
+					{props.seatData && ticketData && (
+						// seat details
+						<main>
+							<div>{`Řada: ${props.seatData.place} Sedadlo: ${props.seatData.place}`}</div>
+							<div>{ticketData?.name}</div>
+							<div>{formatCurrency(ticketData.price)}</div>
+						</main>
+					)}
 					<footer className="flex flex-col">
 						{isInCart ? (
 							<Button disabled variant="destructive" size="sm">
@@ -51,3 +75,7 @@ export const Seat = React.forwardRef<HTMLDivElement, SeatProps>(
 		);
 	},
 );
+
+function getTicketData(ticketID: string, refTicketArray: TicketType) {
+	return refTicketArray.find((element) => element.id === ticketID);
+}
--- src/components/main/SeatingArea.tsx
+++ src/components/main/SeatingArea.tsx
@@ -1,33 +1,18 @@
 import axios from "axios";
 import { Seat } from "../Seat";
 import React, { useEffect, useState } from "react";
+import { SeatRows, TicketType } from "@/types/types";
 
 // Number of rows in venue
 const ROWS = 10;
 // Number of seats in one row
 const COLUMNS = 15;
 
-type TicketType = {
-	id: string;
-	name: string;
-	price: number;
-};
-
-type SeatRows = {
-	seatRow: number;
-	seats: {
-		seatId: string;
-		place: number;
-		ticketTypeId: string;
-		taken?: boolean;
-	}[];
-}[];
-
 export default function SeatingArea() {
-	const [tickets, setTickets] = useState<TicketType | null>(null);
-	const [seatRows, setSeatRows] = useState<SeatRows | null>(null);
+	const [tickets, setTickets] = useState<TicketType | undefined>(undefined);
+	const [seatRows, setSeatRows] = useState<SeatRows | undefined>(undefined);
 	const [loading, setLoading] = useState(true);
-	const [error, setError] = useState<string | null>(null);
+	const [error, setError] = useState<string | undefined>(undefined);
 
 	// fetch tickets and seating data
 	const getTicketsAndSeats = async () => {
@@ -75,23 +60,25 @@ export default function SeatingArea() {
 						Array.from({ length: ROWS * COLUMNS }, (_, i) => {
 							const rowIndex = Math.floor(i / COLUMNS); // calculate current row index
 							const seatInRow = (i % COLUMNS) + 1; // seat position within the row
+							const rowLetter = String.fromCharCode(65 + rowIndex);
 
-							// check if the seat is free
-							const isSeatFree = checkIfSeatIsFree(i, seatRows);
+							// check if the seet is free and get its data
+							const seatData = getSeatData(rowIndex, seatInRow, seatRows);
 
 							return (
 								<React.Fragment key={i}>
 									{/* display the row letter only once for each row */}
 									{seatInRow === 1 && (
-										<span className="m-auto">
-											{String.fromCharCode(65 + rowIndex)}
-										</span> // Convert row index to letter (0 -> 'A', 1 -> 'B', etc.)
+										<span className="m-auto">{rowLetter}</span> // Convert row index to letter (0 -> 'A', 1 -> 'B', etc.)
 									)}
 									{/* seat element */}
 									<Seat
-										// if isSeatFree === false -> disabled === true -> renders disabled seat
-										disabled={!isSeatFree}
+										// if seatData === undefined -> disabled is passing true || if seatData === true -> disabled passing false -> seat is rendered as free
+										disabled={!seatData}
 										seatNumber={seatInRow}
+										seatData={seatData}
+										ticketTypes={tickets}
+										rowLetter={rowLetter}
 									/>
 								</React.Fragment>
 							);
@@ -101,18 +88,15 @@ export default function SeatingArea() {
 	);
 }
 
-// function to compare seating map and fetch data to provide free and taken seats
-function checkIfSeatIsFree(seatIndex: number, seatRows: SeatRows) {
-	const rowIndex = Math.floor(seatIndex / COLUMNS); // calculate current row
-	const placeInRow = (seatIndex % COLUMNS) + 1; // calculate seat position within the row
-
+// check if free seat exists and get ist data from fetched API variable
+function getSeatData(row: number, seatInRow: number, seatRows: SeatRows) {
 	// check if the row exists before accessing its seats
-	if (seatRows[rowIndex] && seatRows[rowIndex].seats) {
-		// Check if any seat from fetched API in the current row matches the calculated placeInRow
-		return seatRows[rowIndex].seats.some((seat) => seat.place === placeInRow);
+	if (seatRows[row] && seatRows[row].seats) {
+		// check if any seat from fetched API in the current row matches the calculated placeInRow
+		return seatRows[row].seats.find((seat) => seat.place === seatInRow);
 	}
-	// if the row doesn't exist or there are no seats in it, return false -> seat is taken
-	return false;
+	// if the row doesn't exist or there are no seats in it, return undefined -> seat is taken
+	return undefined;
 }
 
 function seatingMapSkeleton() {
