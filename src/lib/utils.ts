869480772b2fafc3514b5a42ea579af6c09273df
David Štafa <david.stafa@gmail.com>
2024-11-12 20:58:01 +0100

    feat: currency formatter

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+const CURRENCY_FORMATTER = new Intl.NumberFormat("cs-CZ", {
+	currency: "CZK",
+	style: "currency",
+	minimumFractionDigits: 0,
+});
+
+export function formatCurrency(amount: number) {
+	return CURRENCY_FORMATTER.format(amount);
+}
