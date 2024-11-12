e5a5ad95f0eee487dc8c134fdab81fd92266f7b4
David Štafa <david.stafa@gmail.com>
2024-11-12 17:39:34 +0100

    feat: seating map created

--- src/components/Seat.tsx
+++ src/components/Seat.tsx
@@ -1,36 +1,53 @@
-import { Button } from '@/components/ui/button.tsx';
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover.tsx';
-import { cn } from '@/lib/utils.ts';
-import React from 'react';
+import { Button } from "@/components/ui/button.tsx";
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from "@/components/ui/popover.tsx";
+import { cn } from "@/lib/utils.ts";
+import { X } from "lucide-react";
+import React from "react";
 
 interface SeatProps extends React.HTMLAttributes<HTMLElement> {
+	disabled: boolean | undefined;
+	seatNumber: number;
 }
 
-export const Seat = React.forwardRef<HTMLDivElement, SeatProps>((props, ref) => {
-	const isInCart = false;
-	return (
-		<Popover>
-			<PopoverTrigger>
-				<div className={cn('size-8 rounded-full bg-zinc-100 hover:bg-zinc-200 transition-color', props.className)}
-					ref={ref}>
-					<span className="text-xs text-zinc-400 font-medium">[n]</span>
-				</div>
-			</PopoverTrigger>
-			<PopoverContent>
-				<pre>{JSON.stringify({ seatData: null }, null, 2)}</pre>
-				
-				<footer className="flex flex-col">{
-					isInCart ? (
-						<Button disabled variant="destructive" size="sm">
-							Remove from cart
-						</Button>
-					) : (
-						<Button disabled variant="default" size="sm">
-							Add to cart
-						</Button>
-					)
-				}</footer>
-			</PopoverContent>
-		</Popover>
-	);
-});
+export const Seat = React.forwardRef<HTMLDivElement, SeatProps>(
+	(props, ref) => {
+		const isInCart = false;
+		return (
+			<Popover>
+				<PopoverTrigger disabled={props.disabled} className="m-auto">
+					<div
+						className={cn(
+							"transition-color flex size-8 items-center justify-center rounded-full",
+							props.className,
+							props.disabled
+								? "bg-zinc-200"
+								: "bg-gradient-to-r from-[#f7f5ff] to-[#7f46db] hover:to-[#f7f5ff]",
+						)}
+						ref={ref}
+					>
+						<span className="text-xs font-medium">{props.disabled ? <X size="20px" strokeWidth="1px"/> : props.seatNumber}</span>
+					</div>
+				</PopoverTrigger>
+				<PopoverContent>
+					<pre>{JSON.stringify({ seatData: null }, null, 2)}</pre>
+
+					<footer className="flex flex-col">
+						{isInCart ? (
+							<Button disabled variant="destructive" size="sm">
+								Remove from cart
+							</Button>
+						) : (
+							<Button disabled variant="default" size="sm">
+								Add to cart
+							</Button>
+						)}
+					</footer>
+				</PopoverContent>
+			</Popover>
+		);
+	},
+);
--- src/components/main/SeatingArea.tsx
+++ src/components/main/SeatingArea.tsx
@@ -1,19 +1,130 @@
+import axios from "axios";
 import { Seat } from "../Seat";
+import React, { ReactNode, useEffect, useState } from "react";
+
+// Number of rows in venue
+const ROWS = 10;
+// Number of seats in one row
+const COLUMNS = 15;
+
+type TicketType = {
+	id: string;
+	name: string;
+	price: number;
+};
+
+type SeatRows = {
+	seatRow: number;
+	seats: {
+		seatId: string;
+		place: number;
+		ticketTypeId: string;
+		taken?: boolean;
+	}[];
+}[];
 
 export default function SeatingArea() {
+	const [tickets, setTickets] = useState<TicketType | null>(null);
+	const [seatRows, setSeatRows] = useState<SeatRows | null>(null);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState<string | null>(null);
+
+	// fetch tickets and seating data
+	const getTicketsAndSeats = async () => {
+		try {
+			const res = await axios.get(
+				"https://nfctron-frontend-seating-case-study-2024.vercel.app/event-tickets?eventId=3348b37f-6ee2-4513-ac40-b55eb676319b",
+			);
+			setTickets(res.data.ticketTypes);
+			setSeatRows(res.data.seatRows);
+		} catch (err) {
+			setError("Something went wrong. Please try again later.");
+			console.error(err);
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	useEffect(() => {
+		getTicketsAndSeats();
+	}, []);
+
+	if (error) return <p>{error}</p>;
+
 	return (
-		// seating card
-		<div
-			className="bg-white rounded-md grow grid p-3 self-stretch shadow-sm"
-			style={{
-				gridTemplateColumns: "repeat(auto-fill, minmax(40px, 1fr))",
-				gridAutoRows: "40px",
-			}}
-		>
-			{/* seating map */}
-			{Array.from({ length: 100 }, (_, i) => (
-				<Seat key={i} />
-			))}
+		// venue card
+		<div className="grow rounded-md bg-white p-3 shadow-sm">
+			{/* stage div */}
+			<div className="flex items-center">
+				<span className="mb-4 w-full border-b pb-2 text-center">Stage</span>
+			</div>
+			{/* seating div */}
+			<div
+				className="grid h-fit"
+				style={{
+					gridTemplateColumns: `repeat(${COLUMNS + 1}, 1fr)`,
+					gridAutoRows: "40px",
+				}}
+			>
+				{loading
+					? seatingMapSkeleton()
+					: 
+						// seating map
+						seatRows &&
+						Array.from({ length: ROWS * COLUMNS }, (_, i) => {
+							const rowIndex = Math.floor(i / COLUMNS); // calculate current row index
+							const seatInRow = (i % COLUMNS) + 1; // seat position within the row
+
+							// check if the seat is taken
+							const isSeatTaken = checkIfSeatIsTaken(i, seatRows);
+
+							return (
+								<>
+									{/* display the row letter only once for each row */}
+									{seatInRow === 1 && (
+										<span className="m-auto">
+											{String.fromCharCode(65 + rowIndex)}
+										</span> // Convert row index to letter (0 -> 'A', 1 -> 'B', etc.)
+									)}
+									{/* seat element */}
+									<Seat
+										key={i}
+										disabled={!isSeatTaken}
+										seatNumber={seatInRow}
+									/>
+								</>
+							);
+						})}
+			</div>
 		</div>
 	);
+}
+
+function checkIfSeatIsTaken(seatIndex: number, seatRows: SeatRows) {
+	const rowIndex = Math.floor(seatIndex / COLUMNS); // calculate current row
+	const placeInRow = (seatIndex % COLUMNS) + 1; // calculate seat position within the row
+
+	// check if the row exists before accessing its seats
+	if (seatRows[rowIndex] && seatRows[rowIndex].seats) {
+		return seatRows[rowIndex].seats.some((seat) => seat.place === placeInRow);
+	}
+	return false;
+}
+
+function seatingMapSkeleton() {
+	return Array.from({ length: ROWS * COLUMNS }, (_, i) => {
+		const rowIndex = Math.floor(i / COLUMNS); // calculate current row index
+		const seatInRow = (i % COLUMNS) + 1; // seat position within the row
+
+		return (
+			<>
+				{/* display the row letter only once for each row */}
+				{seatInRow === 1 && (
+					<span className="m-auto">{String.fromCharCode(65 + rowIndex)}</span> // Convert row index to letter (0 -> 'A', 1 -> 'B', etc.)
+				)}
+				{/* seat skeleton element*/}
+				<div key={i} className="size-8 animate-pulse bg-zinc-300 rounded-full" />
+			</>
+		);
+	});
 }
